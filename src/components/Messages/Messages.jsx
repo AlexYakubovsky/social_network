f8de9ff501e3a6da45f5329ccce8a1fc9a672388
Alexander <alex.yakubovsky.g@gmail.com>
2020-06-21 23:20:50 +0300

    Add content to Message

--- src/components/Messages/Messages.jsx
+++ src/components/Messages/Messages.jsx
@@ -1,10 +1,38 @@
 import React from "react";
-// import styles from "./Messages.module.css";
+import styles from "./Messages.module.css";
 
 const Messages = () => {
     return (
-        <div>
-            messages
+        <div className={styles.messages}>
+            <div className={styles.messages_users}>
+                <div className={styles.messages_users__user}>
+                    Alex
+                </div>
+                <div className={styles.messages_users__user}>
+                    Kirill
+                </div>
+                <div className={styles.messages_users__user}>
+                    Evgeny
+                </div>
+                <div className={styles.messages_users__user}>
+                    Olga
+                </div>
+            </div>
+
+            <div className={styles.messages_dialogs}>
+                <div className={styles.messages_dialogs__message}>
+                    hello
+                </div>
+                <div className={styles.messages_dialogs__message}>
+                    how are you?
+                </div>
+                <div className={styles.messages_dialogs__message}>
+                    what do think about this?
+                </div>
+                <div className={styles.messages_dialogs__message}>
+                    are you sure?
+                </div>
+            </div>
         </div>
     )
 };

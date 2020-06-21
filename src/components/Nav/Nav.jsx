833efc2bcd89d79b9a389dec2b0f4b0b3e138af0
Alexander <alex.yakubovsky.g@gmail.com>
2020-06-21 19:18:01 +0300

    Add NavLink

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,13 +1,20 @@
 import React from "react";
 import styles from "./Nav.module.css"
+import {NavLink} from "react-router-dom";
 
 const Nav = () => {
     return (
         <nav className={styles.nav}>
             <ul>
-                <li><a href='/profile'>profile</a></li>
-                <li><a href='/messages'>messages</a></li>
-                <li><a href='/friends'>friends</a></li>
+                <li>
+                    <NavLink to='/profile' activeClassName={styles.active}>profile</NavLink>
+                </li>
+                <li>
+                    <NavLink to='/messages' activeClassName={styles.active}>messages</NavLink>
+                </li>
+                <li>
+                    <NavLink to='/friends' activeClassName={styles.active}>friends</NavLink>
+                </li>
             </ul>
         </nav>
     );

abda606d73c8da9cb199144f0d384c9a3955519d
Alexander <alex.yakubovsky.g@gmail.com>
2020-06-30 23:57:40 +0300

    Add ref to NewPost and Messages

--- src/components/Messages/Messages.jsx
+++ src/components/Messages/Messages.jsx
@@ -7,10 +7,23 @@ const Messages = props => {
     let userComponent = props.state.userData.map(v => <User id={v.id} name={v.name}/>);
     let dialogComponent = props.state.dialogData.map(v => <Dialog message={v.message}/>);
 
+    let texareaElem = React.createRef();
+
+    let sendMessage = () => {
+        let messageValue = texareaElem.current.value;
+        alert(messageValue)
+    };
+
     return (
-        <div className={styles.messages}>
-            <div>{userComponent}</div>
-            <div>{dialogComponent}</div>
+        <div>
+            <div className={styles.messages}>
+                <div>{userComponent}</div>
+                <div>{dialogComponent}</div>
+            </div>
+            <div>
+                <textarea ref={texareaElem}/>
+                <button onClick={sendMessage}>Send message</button>
+            </div>
         </div>
     )
 };
--- src/components/Profile/NewPost/NewPost.jsx
+++ src/components/Profile/NewPost/NewPost.jsx
@@ -2,10 +2,17 @@ import React from "react";
 // import styles from "./NewPost.module.css"
 
 const NewPost = () => {
+    let textareaElem = React.createRef();
+
+    let addPost = () => {
+        let newPostText = textareaElem.current.value;
+        alert(newPostText);
+    };
+
     return (
         <div>
-            <textarea></textarea>
-            <button>Add post</button>
+            <textarea ref={textareaElem}/>
+            <button onClick={addPost}>Add post</button>
         </div>
     );
 };

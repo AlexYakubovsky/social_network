75e4d8c9715f09dc10ad33ed698480a83343050c
Alexander <alex.yakubovsky.g@gmail.com>
2020-07-17 01:15:22 +0300

    Connected React-Redux library. Add connect to NewPostContainer

--- src/components/Profile/NewPost/NewPostContainer.jsx
+++ src/components/Profile/NewPost/NewPostContainer.jsx
@@ -1,17 +1,21 @@
 import React from "react";
 import {addPost, changePostValue} from "../../../redux/profileReducer";
 import NewPost from "./NewPost";
+import {connect} from "react-redux";
 
-const NewPostContainer = props => {
-    let newPostValue = value => {
-        props.dispatch(changePostValue(value));
-    };
-
-    let addNewPost = () => props.dispatch(addPost());
-
-    return (
-        <NewPost newPostValue={newPostValue} addNewPost={addNewPost} postValue={props.postValue}/>
-    );
+const mapStateToProps = state => {
+    return {
+        postValue: state.profilePage.postValue
+    }
 };
+
+const mapDispatchToProps = dispatch => {
+    return {
+        newPostValue: value => dispatch(changePostValue(value)),
+        addNewPost: () => dispatch(addPost())
+    }
+};
+
+const NewPostContainer = connect(mapStateToProps, mapDispatchToProps)(NewPost);
 
 export default NewPostContainer;
--- src/index.js
+++ src/index.js
@@ -5,13 +5,16 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import {BrowserRouter} from "react-router-dom";
 import store from "./redux/store";
+import {Provider} from "react-redux";
 
 export let rerenderAllTree = () => {
     ReactDOM.render(
         <React.StrictMode>
             <BrowserRouter>
-                <App state={store.getState()}
-                     dispatch={store.dispatch.bind(store)}/>
+                <Provider store={store}>
+                    <App state={store.getState()}
+                         dispatch={store.dispatch.bind(store)}/>
+                </Provider>
             </BrowserRouter>
         </React.StrictMode>,
         document.getElementById('root')

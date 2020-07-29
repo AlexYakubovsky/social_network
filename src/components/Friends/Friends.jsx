080211443bb378721a43bcb0e8dd043a1b49f281
Alexander <alex.yakubovsky.g@gmail.com>
2020-07-30 00:18:50 +0300

    Add classComponent to Friends

--- src/components/Friends/Friends.jsx
+++ src/components/Friends/Friends.jsx
@@ -3,35 +3,38 @@ import styles from "./Friends.module.css";
 import * as axios from "axios";
 import userLogo from "../../assets/images/user-logo.png"
 
-const Friends = props => {
-    if (props.users.length === 0) {
-        axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => props.setUsers(response.data.items));
+class Friends extends React.Component {
+    constructor(props) {
+        super(props);
+
+        axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => this.props.setUsers(response.data.items));
     }
 
-    return (
-        <div>
-            {props.users.map(v => {
-                return (
-                    <div key={v.id} className={styles.friends}>
-                        <img alt='' src={v.photos.small ? v.photos.small : userLogo}/>
-                        <div>
+    render() {
+        return (
+            <div>
+                {this.props.users.map(v => {
+                    return (
+                        <div key={v.id} className={styles.friends}>
+                            <img alt='' src={v.photos.small ? v.photos.small : userLogo}/>
                             <div>
-                                {v.name}
+                                <div>
+                                    {v.name}
+                                </div>
+                                <div>
+                                    {v.status}
+                                </div>
                             </div>
                             <div>
-                                {v.status}
+                                {v.followed ? <button onClick={() => this.props.unfollowUser(v.id)}>Unfollow</button> :
+                                    <button onClick={() => this.props.followUser(v.id)}>Follow</button>}
                             </div>
                         </div>
-                        <div>
-                            {v.followed ? <button onClick={() => props.unfollowUser(v.id)}>Unfollow</button> :
-                                <button onClick={() => props.followUser(v.id)}>Follow</button>}
-                        </div>
-                    </div>
-
-                )
-            })}
-        </div>
-    )
-};
+                    )
+                })}
+            </div>
+        )
+    }
+}
 
 export default Friends;

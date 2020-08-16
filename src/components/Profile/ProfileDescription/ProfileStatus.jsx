b569d75895393f63e8619cedafe790c27bbffa62
Alexander <alex.yakubovsky.g@gmail.com>
2020-08-16 19:25:06 +0300

    Add componentDidMount to ProfileStatus

--- src/components/Profile/ProfileDescription/ProfileStatus.jsx
+++ src/components/Profile/ProfileDescription/ProfileStatus.jsx
@@ -6,40 +6,30 @@ class ProfileStatus extends React.Component {
         userStatus: this.props.userStatus
     };
 
-    activeEditMode = () => {
-        this.setState({
-            editMode: true
-        })
-    };
+    activeEditMode = () => this.setState({editMode: true});
 
     deactiveEditMode = () => {
-        this.setState({
-            editMode: false
-        });
-
-        this.props.updateStatus(this.state.userStatus)
+        this.setState({editMode: false});
+        this.props.updateStatus(this.state.userStatus);
     };
 
-    onChangeStatus = e => {
-        this.setState({
-            userStatus: e.target.value
-        })
-    };
+    onChangeStatus = e => this.setState({userStatus: e.target.value});
+
+    componentDidUpdate(prevProps) {
+        if (prevProps.status !== this.props.status) {
+            this.setState({userStatus: this.props.userStatus})
+        }
+    }
 
     render() {
         return (
-            <>
+            <div>
                 {!this.state.editMode ?
-                    <div>
-                        <span
-                            onClick={this.activeEditMode}>{this.props.userStatus || 'Enter your status'}</span>
-                    </div> :
-                    <div>
-                        <input onChange={this.onChangeStatus} onBlur={this.deactiveEditMode}
-                               value={this.state.userStatus} autoFocus={true}/>
-                    </div>
+                    <span onClick={this.activeEditMode}>{this.state.userStatus || 'Enter your status'}</span> :
+                    <input onChange={this.onChangeStatus} onBlur={this.deactiveEditMode} value={this.state.userStatus}
+                           autoFocus={true}/>
                 }
-            </>
+            </div>
         )
     }
 }
